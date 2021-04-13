60679d9dd59803dcbb3506ee1446051562f1fe12
han <igoman2@ajou.ac.kr>
2021-04-13 09:38:11 +0900

    Fragment

---
+++ android/app/src/main/java/org/tensorflow/lite/examples/detection/HomeActivity.java
@@ -0,0 +1,71 @@
+package org.tensorflow.lite.examples.detection;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class HomeActivity extends FragmentActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    private FragmentManager fm;
+    private FragmentTransaction ft;
+    private Fragment1 fragment1;
+    private Fragment2 fragment2;
+//    private Fragment3 fragment3;
+//    private Fragment4 fragment4;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        bottomNavigationView = findViewById(R.id.bottomNavi);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action1:
+                        setFragment(0);
+                        break;
+                    case R.id.action2:
+                        setFragment(1);
+                        break;
+                    case R.id.action3:
+                        setFragment(2);
+                        break;
+                    case R.id.action4:
+                        setFragment(3);
+                        break;
+                }
+                return true;
+            }
+        });
+
+        fragment1 = new Fragment1();
+        fragment2 = new Fragment2();
+        setFragment(0);
+
+    }
+    private void setFragment(int n) {
+        fm = getSupportFragmentManager();
+        ft = fm.beginTransaction(); //프래그먼트 교체
+        switch (n) { //activity_initial 에 있는 FrameLayout 에 프래그먼트 삽입
+            case 0:
+                ft.replace(R.id.main_frame, fragment1);
+                ft.commit(); //저장을 의미
+                break;
+            case 1:
+                ft.replace(R.id.main_frame, fragment2);
+                ft.commit(); //저장을 의미
+                break;
+        }
+    }
+}
--- android/app/src/main/java/org/tensorflow/lite/examples/detection/LoginActivity.java
+++ android/app/src/main/java/org/tensorflow/lite/examples/detection/LoginActivity.java
@@ -21,7 +21,7 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View view){
                 Toast.makeText(getApplicationContext(),"로그인 되었습니다.", Toast.LENGTH_LONG).show();
-                Intent intent = new Intent(LoginActivity.this, DetectorActivity.class);
+                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
                 startActivity(intent);
             }
         });
